60d68e870fab1ca5ad7307fbe869119395982843
Sachin Kumar <sachinkmr375@gmail.com>
2016-09-26 22:04:44 +0530

    Find first non repeating char in string

---
+++ Solution.java
@@ -0,0 +1,46 @@
+package Java_Codes;
+
+import java.util.LinkedHashMap;
+
+public class Solution {
+
+	public static String s = "GeeksforGeeks";
+
+	public static LinkedHashMap<Character, Integer> hashMap = new LinkedHashMap<>();
+
+	public static void main(String[] args) {
+
+		int i = 0;
+
+		while (i < s.length()) {
+
+			performOp(s.charAt(i));
+
+			i++;
+		}
+
+		printCh();
+
+	}
+
+	public static void performOp(char ch) {
+
+		if (hashMap.containsKey(ch))
+			hashMap.put(ch, hashMap.get(ch) + 1);
+		else
+			hashMap.put(ch, 1);
+
+	}
+
+	public static void printCh() {
+
+		for (Character character : hashMap.keySet()) {
+
+			if (hashMap.get(character) == 1) {
+				System.out.println(character);
+				break;
+			}
+		}
+	}
+
+}
